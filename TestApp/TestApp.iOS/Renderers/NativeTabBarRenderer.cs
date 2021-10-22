2dc593a7f17c8b22702202f66c7c27cbbd9391f7
Julian Baumann <julian-baumann@outlook.com>
2021-10-22 09:24:10 +0200

    Uncommented some code

--- TestApp/TestApp.iOS/Renderers/NativeTabBarRenderer.cs
+++ TestApp/TestApp.iOS/Renderers/NativeTabBarRenderer.cs
@@ -3,20 +3,20 @@ using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
-// [assembly: ExportRenderer(typeof(TabbedPage), typeof(NativeTabBarRenderer))]
-// namespace TestApp.iOS.Renderers
-// {
-//     public class NativeTabBarRenderer : TabbedRenderer
-//     {
-//
-//         public override void ViewWillAppear(bool animated)
-//         {
-//             base.ViewWillAppear(animated);
-//
-//             TabBar.Translucent = true;
-//             EdgesForExtendedLayout = UIRectEdge.Bottom;
-//             ViewController.EdgesForExtendedLayout = UIRectEdge.Bottom;
-//             ViewController.ExtendedLayoutIncludesOpaqueBars = true;
-//         }
-//     }
-// }
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(NativeTabBarRenderer))]
+namespace TestApp.iOS.Renderers
+{
+    public class NativeTabBarRenderer : TabbedRenderer
+    {
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            TabBar.Translucent = true;
+            EdgesForExtendedLayout = UIRectEdge.Bottom;
+            ViewController.EdgesForExtendedLayout = UIRectEdge.Bottom;
+            ViewController.ExtendedLayoutIncludesOpaqueBars = true;
+        }
+    }
+}
